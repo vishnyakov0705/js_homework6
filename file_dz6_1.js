384d1e25b38c1c9ad95284c45429e04435dc36a3
vishnyakov0705 <vishnyakov.v.i.0705@gmail.com>
2024-06-11 17:57:56 +0300

    додавання файла file_dz6_1

---
+++ file_dz6_1.js
@@ -0,0 +1,12 @@
+function myBlend(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]]; // Заміна елементів
+  }
+}
+
+// Приклад використання
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("Заданий масив: ", arr);
+myBlend(arr);
+console.log("Перемішаний масив: ", arr);
